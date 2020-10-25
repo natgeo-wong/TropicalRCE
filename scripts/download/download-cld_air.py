e9077dea61fadfd7ffe649983ef54d2c44c8603a
Nathanael Wong <natgeo.wong@outlook.com>
2020-10-25 12:09:43 -0400

    Adding downloads for cloud level monthly avg to get overall profile

--- scripts/download/download-cld_air.py
+++ scripts/download/download-cld_air.py
@@ -39,3 +39,22 @@ for yr in range(1979,2020):
                 'area': [30, 0, -30, 360],
             },
             datadir + 'era5-TRPx0.25-cld_air-' + str(lvl) + 'hPa-' + str(yr) + '.nc')
+
+for yr in range(1979,2020):
+    c.retrieve(
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'format': 'netcdf',
+            'product_type': 'monthly_averaged_reanalysis_by_hour_of_day',
+            'variable': 'fraction_of_cloud_cover',
+            'pressure_level': [
+                50,70,100,125,150,175,200,225,250,300,
+                350,400,450,500,550,600,650,700,750,775,
+                800,825,850,875,900,925,950,975,1000
+            ],
+            'year': yr,
+            'month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            'area': [30, 0, -30, 360],
+            'time': '00:00',
+        },
+        datadir + 'era5-TRPx0.25-cld_air-' + str(yr) + '.nc')
