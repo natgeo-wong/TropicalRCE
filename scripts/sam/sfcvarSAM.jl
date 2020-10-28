cb7be67a0a344fe99d435b1b30c1f70941f541c9
Nathanael Wong <natgeo.wong@outlook.com>
2020-10-28 14:20:52 -0400

    Debug sfc extract

--- scripts/sam/sfcvarSAM.jl
+++ scripts/sam/sfcvarSAM.jl
@@ -9,12 +9,18 @@ sfcsummary("PREC","DTP2M","sst300d8",[
     "sst302d0","sst302d5","sst303d0"
 ])
 
-sebsummary("PREC","DRY2M","sst299d7",[
+sfcsummary("PREC","DRY2M","sst299d7",[
     "sst297d0","sst298d0","sst299d0","sst299d5",
     "sst299d7","sst300d0","sst300d5","sst301d0",
     "sst301d5","sst302d0"
 ])
 
-sebsummary("PREC","WPW2M","sst302d4",[
+sfcsummary("PREC","WPW2M","sst302d4",[
     "sst301d0","sst302d0","sst302d4","sst302d5","sst303d0"
 ])
+
+sfcsummary("PREC","IPW2M","sst301d9",["sst301d9"])
+sfcsummary("PREC","DTP1M","sst300d8",["sst300d8"])
+sfcsummary("PREC","IPW1M","sst301d9",["sst301d9"])
+sfcsummary("PREC","WPW1M","sst302d4",["sst302d4"])
+sfcsummary("PREC","DRY1M","sst299d7",["sst299d7"])
--- src/SAM.jl
+++ src/SAM.jl
@@ -2,7 +2,11 @@ using NCDatasets
 using PrettyTables
 using Statistics
 
-function sfcextract(experiment::AbstractString, config::AbstractString)
+function sfcextract(
+    variable::AbstractString,
+    experiment::AbstractString,
+    config::AbstractString
+)
 
     dfnc = joinpath(datadir(experiment,config,"OUT_STAT","RCE_DiConv-$(experiment).nc"))
     rce = NCDataset(dfnc)
@@ -20,17 +24,22 @@ function sfcsummary(
     experiment::AbstractString,
     control::AbstractString,
     config::AbstractVector{<:AbstractString},
-    days::Integer=100)
+    days::Integer=100
+)
 
     nconfig = length(config)
-    vartable = zeros(nconfig,3)
-    tc,varc = sebextract(variable,experiment,control)
-    tstepc = round(Integer,(length(t) - 1) / (t[end] - t[1]))
-    varc = mean(reshape(varc,tstep,:)[:,(end-days+1):end])
+    vartable = zeros(nconfig+1,3)
+    tc,varc = sfcextract(variable,experiment,control)
+    tstepc = round(Integer,(length(tc) - 1) / (tc[end] - tc[1]))
+    varc = mean(reshape(varc,tstepc,:)[:,(end-days+1):end])
+
+    vartable[1,1] = parse(Float32,replace(replace(control,"sst"=>""),"d"=>"."))
+    vartable[1,2] = varc
+    vartable[1,3] = 0
 
     for icon = 1 : nconfig
 
-        t,var = sebextract(experiment,config[icon])
+        t,var = sfcextract(variable,experiment,config[icon])
 
         tstep = round(Integer,(length(t) - 1) / (t[end] - t[1]))
 
@@ -43,7 +52,7 @@ function sfcsummary(
     head = ["SAM SST / K","$variable","Config - Control"];
 
     pretty_table(
-        round.(sebtable,digits=2),head,
+        round.(vartable,digits=2),head,
         alignment=[:c,:c,:c],
         tf=compact
     );
