12be70d9020ad31ffb3e72582836068573c2d1ee
Nathanael Wong <natgeo.wong@outlook.com>
2020-11-30 00:25:08 -0500

    Debugging

--- scripts/diurnal/compare.jl
+++ scripts/diurnal/compare.jl
@@ -18,7 +18,7 @@ function sam3D(experiment::AbstractString,config::AbstractString;days::Integer=1
     vcld = cld .- mcld
     mcld = dropdims(mcld,dims=2)
 
-    return p,mcld,vcld
+    return t,p,mcld,vcld
 
 end
 
@@ -43,12 +43,12 @@ mocca_DTP = mean(occa_DTP[1:24,:],dims=1)
 vocca_DTP = occa_DTP .- mocca_DTP
 mocca_DTP = dropdims(mocca_DTP,dims=1)
 
-p,mocca_RCE,vocca_RCE = sam3D("Control2DHR","DTP1M")
-p,mocca_LSV,vocca_LSV = sam3D("LSVert","DTP1M")
-p,mocca_SHR,vocca_SHR = sam3D("Shear","DTP1M")
-# p,mocca_WTG,vocca_WTG = sam3D("WTG","DTP1M")
+t,p_RCE,mocca_RCE,vocca_RCE = sam3D("Control2DHR","DTP1M")
+_,p_LSV,mocca_LSV,vocca_LSV = sam3D("LSVert","DTP1M")
+_,p_SHR,mocca_SHR,vocca_SHR = sam3D("Shear","DTP1M")
+# _,p_WTG,mocca_WTG,vocca_WTG = sam3D("WTG","DTP1M")
 
-arr = [[0,0,1,2,2,2,0,0][3,4,4,4,5,6,6,6],[7,8,8,8,9,10,10,10]]
+arr = [[0,0,1,2,2,2,0,0],[3,4,4,4,5,6,6,6],[7,8,8,8,9,10,10,10]]
 pplt.close(); f,axs = pplt.subplots(arr,aspect=0.5,axwidth=1,wspace=(0,0,0,nothing,0,0,0));
 clvl = [-10,-5,-2,-1,-0.5,-0.2,0.2,0.5,1,2,5,10]
 
@@ -56,20 +56,20 @@ axs[1].plot(mocca_DTP[:],lvl)
 c = axs[2].contourf(0:24,lvl,vocca_DTP',cmap="drywet",levels=clvl,extend="both")
 axs[1].format(urtitle="ERA5",suptitle="Diurnal Cycle of Cloud Cover / %")
 
-axs[3].plot(mocca_RCE[:],lvl)
-axs[4].contourf(0:24,lvl,vocca_RCE',cmap="drywet",levels=clvl,extend="both")
+axs[3].plot(mocca_RCE[:],p_RCE)
+axs[4].contourf(t,p_RCE,vocca_RCE,cmap="drywet",levels=clvl,extend="both")
 axs[3].format(urtitle="RCE")
 
-axs[5].plot(mocca_LSV[:],lvl)
-axs[6].contourf(0:24,lvl,vocca_LSV',cmap="drywet",levels=clvl,extend="both")
+axs[5].plot(mocca_LSV[:],p_LSV)
+axs[6].contourf(t,p_LSV,vocca_LSV,cmap="drywet",levels=clvl,extend="both")
 axs[5].format(urtitle="LSVert")
 
-axs[7].plot(mocca_SHR[:],lvl)
-axs[8].contourf(0:24,lvl,vocca_SHR',cmap="drywet",levels=clvl,extend="both")
+axs[7].plot(mocca_SHR[:],p_SHR)
+axs[8].contourf(t,p_SHR,vocca_SHR,cmap="drywet",levels=clvl,extend="both")
 axs[7].format(urtitle="Shear")
 
 # axs[9].plot(mocca_WTG[:],lvl)
-# axs[10].contourf(0:24,lvl,vocca_WTG',cmap="drywet",levels=clvl,extend="both")
+# axs[10].contourf(t,p_WTG,vocca_WTG',cmap="drywet",levels=clvl,extend="both")
 axs[9].format(urtitle="WTG")
 
 for ii = 1 : 10
