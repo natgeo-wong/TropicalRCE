c072bf9814cd1c71b56af7336e03dfe1e4295370
Nathanael Wong <natgeo.wong@outlook.com>
2020-12-18 01:20:12 -0500

    Adding new plot for Earth Skin Temperature

--- scripts/reanalysis/plotskt.jl
+++ scripts/reanalysis/plotskt.jl
@@ -13,7 +13,7 @@ pplt = pyimport("proplot");
 
 include(srcdir("reanalysis.jl"))
 
-function plottmp(
+function plotskin(
     sbin::AbstractRange,
     lbin::AbstractRange,
     lvls::AbstractRange,
@@ -21,16 +21,10 @@ function plottmp(
     verbose::Bool=false
 )
 
-    ds = NCDataset(datadir("reanalysis/era5-TRPx0.25-sst-sfc.nc"))
+    ds = NCDataset(datadir("reanalysis/era5-TRPx0.25-skt-sfc.nc"))
     lon = ds["longitude"][:]
     lat = ds["latitude"][:]
-    sst = ds["sst"][:]*1
-    close(ds)
-
-    ds = NCDataset(datadir("reanalysis/era5-TRPx0.25-t2m-sfc.nc"))
-    lon = ds["longitude"][:]
-    lat = ds["latitude"][:]
-    t2m = ds["t2m"][:]*1
+    skt = ds["skt"][:]*1
     close(ds)
 
     ds = NCDataset(datadir("reanalysis/era5-TRPx0.25-lsm-sfc.nc"))
@@ -40,17 +34,15 @@ function plottmp(
     psbin = (sbin[2:end].+sbin[1:(end-1)])/2
     plbin = (lbin[2:end].+lbin[1:(end-1)])/2
 
-    sst[ismissing.(sst)] .= NaN; sst = convert.(Float64,sst)
+    _,sbin_DTP,_,savg_DTP = bindatasfc([15,-15,360,0],sbin,skt,lon,lat,lsm)
+    _,sbin_IPW,_,savg_IPW = bindatasfc([15,-15,180,90],sbin,skt,lon,lat,lsm)
+    _,sbin_WPW,_,savg_WPW = bindatasfc([5,-10,180,135],sbin,skt,lon,lat,lsm)
+    _,sbin_DRY,_,savg_DRY = bindatasfc([5,-5,275,180],sbin,skt,lon,lat,lsm)
 
-    _,sbin_DTP,_,savg_DTP = bindatasfc([15,-15,360,0],sbin,sst,lon,lat,lsm)
-    _,sbin_IPW,_,savg_IPW = bindatasfc([15,-15,180,90],sbin,sst,lon,lat,lsm)
-    _,sbin_WPW,_,savg_WPW = bindatasfc([5,-10,180,135],sbin,sst,lon,lat,lsm)
-    _,sbin_DRY,_,savg_DRY = bindatasfc([5,-5,275,180],sbin,sst,lon,lat,lsm)
-
-    lbin_DTP,_,lavg_DTP,_ = bindatasfc([15,-15,360,0],lbin,t2m,lon,lat,lsm)
-    lbin_IPW,_,lavg_IPW,_ = bindatasfc([15,-15,180,90],lbin,t2m,lon,lat,lsm)
-    lbin_WPW,_,lavg_WPW,_ = bindatasfc([5,-10,180,135],lbin,t2m,lon,lat,lsm)
-    lbin_DRY,_,lavg_DRY,_ = bindatasfc([5,-5,275,180],lbin,t2m,lon,lat,lsm)
+    lbin_DTP,_,lavg_DTP,_ = bindatasfc([15,-15,360,0],lbin,skt,lon,lat,lsm)
+    lbin_IPW,_,lavg_IPW,_ = bindatasfc([15,-15,180,90],lbin,skt,lon,lat,lsm)
+    lbin_WPW,_,lavg_WPW,_ = bindatasfc([5,-10,180,135],lbin,skt,lon,lat,lsm)
+    lbin_DRY,_,lavg_DRY,_ = bindatasfc([5,-5,275,180],lbin,skt,lon,lat,lsm)
 
     coord = readdlm(datadir("GLB-i.txt"),comments=true,comment_char='#')
     x = coord[:,1]; y = coord[:,2];
@@ -63,8 +55,7 @@ function plottmp(
     pplt.close(); arr = [[1,1],[2,3]];
     f,axs = pplt.subplots(arr,aspect=6,axwidth=6,sharey=0);
 
-    c1 = axs[1].contourf(lon,lat,t2m',levels=lvls,extend=extd);
-    c1 = axs[1].contourf(lon,lat,sst',levels=lvls,extend=extd);
+    c1 = axs[1].contourf(lon,lat,skt',levels=lvls,extend=extd);
     axs[1].plot(x,y,c="k",lw=0.2)
     axs[1].plot(DTP[1],DTP[2],c="b",lw=1)
     axs[1].plot(IPW[1],IPW[2],c="r",lw=1)
@@ -73,7 +64,7 @@ function plottmp(
     axs[1].format(
         ylim=(-30,30),
         xlim=(0,360),xlocator=[0:60:360],
-        suptitle="Surface Temperature / K"
+        suptitle="Skin Temperature / K"
     )
     f.colorbar(c1,loc="r")
 
@@ -102,16 +93,16 @@ function plottmp(
     end
 
     mkpath(plotsdir("REANALYSIS"))
-    f.savefig(plotsdir("REANALYSIS/t_sfc.png"),transparent=false,dpi=200)
+    f.savefig(plotsdir("REANALYSIS/t_skt.png"),transparent=false,dpi=200)
 
     if verbose
-        @info """Average Sea Surface Temperature (1979-2019):
+        @info """Average OCEAN $(BOLD(uppercase("SKIN TEMPERATURE"))) (1979-2019):
           $(BOLD("DTP (Deep Tropics):"))          $(@sprintf("%0.2f",savg_DTP)) K
           $(BOLD("IPW (Indo-Pacific Warmpool):")) $(@sprintf("%0.2f",savg_IPW)) K
           $(BOLD("WPW (West Pacific Warmpool):")) $(@sprintf("%0.2f",savg_WPW)) K
           $(BOLD("DRY (Dry Pacific):"))           $(@sprintf("%0.2f",savg_DRY)) K
 
-        Average 2m Land Surface Temperature (1979-2019):
+        Average LAND $(BOLD(uppercase("SKIN TEMPERATURE"))) (1979-2019):
           $(BOLD("DTP (Deep Tropics):"))          $(@sprintf("%0.2f",lavg_DTP)) K
           $(BOLD("IPW (Indo-Pacific Warmpool):")) $(@sprintf("%0.2f",lavg_IPW)) K
           $(BOLD("WPW (West Pacific Warmpool):")) $(@sprintf("%0.2f",lavg_WPW)) K
@@ -120,4 +111,4 @@ function plottmp(
 
 end
 
-plottmp(294:0.05:304,280:0.05:310,295:0.5:305,"both",verbose=true)
+plotskin(294:0.05:304,280:0.05:310,295:0.5:305,"both",verbose=true)
